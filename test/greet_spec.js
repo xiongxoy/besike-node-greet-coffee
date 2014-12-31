49f22a8dafccabc0d266940a64fd6a2aed8300f4
xiongxoy <xiong.xoy@gmail.com>
2014-12-31 20:52:12 +0800

    Add Tests

---
+++ test/greet_spec.js
@@ -0,0 +1,19 @@
+var greet = require('greet');
+
+describe('greet', function () {
+  var name = 'ZhouXiong',
+      drunk = false;
+
+  it ('should greet a person by name', function () {
+    drunk = false;
+    var result = greet(name, drunk);
+    expect(result).to.contain(name).and.not.contain('sexy');
+  });
+
+  it ('should greet a person flirtatiously if drunk', function () {
+    drunk = true;
+    var result = greet(name, drunk);
+    expect(result).to.contain(name).and.contain('sexy');
+  });
+
+});
